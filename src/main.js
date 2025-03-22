c7610a64965ca67060962a9ca7b41c73d7737457
royyanbach <royyan.bachtiar@gmail.com>
2025-03-22 16:14:05 +0700

    Add dark mode

--- src/main.js
+++ src/main.js
@@ -16,4 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   feather.replace();
+
+  const darkModeToggle = document.getElementById('darkModeToggle');
+
+  if (localStorage.theme === 'dark' ||
+     (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+
+  darkModeToggle.addEventListener('click', () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }
+    feather.replace();
+  });
 })
